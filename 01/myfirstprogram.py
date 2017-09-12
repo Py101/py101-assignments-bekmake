22d8aa84eefbf419dd7e79f01e8bad91da550201
Malik Bekmurat <malik.bekmurat@nu.edu.kz>
2017-09-12 14:12:06 +0600

    add something to mybio file

--- 01/myfirstprogram.py
+++ 01/myfirstprogram.py
@@ -4,9 +4,9 @@ my first program in python
 
 import sys
 numbers = sys.argv[1:]
-a,b = int(numbers[0]),int(numbers[1])
+a,b = int(numbers[7]),int(numbers[3])
 
 print("the number you've passed are:")
 print(a,b)
 
-print("a+b=", a+b)
+print("a+b=", a%b)
